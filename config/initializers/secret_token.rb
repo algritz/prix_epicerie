bc906b44919ecf0c7e2b7be214e077a9466bbdde
David Cloutier <algritz@videotron.ca>
2010-12-14 11:20:17 -0500

    Initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrixEpicerie::Application.config.secret_token = 'af982481e19da7d76d80d6489239035a43fcc2848fedca4083d1dbb1fea409d6db55ebbad8ed7ff32dab5de5afd9248b4532ddd2b88a6701daeb7e3ec394e3f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PrixEpicerie::Application.config.session_store :cookie_store, :key => '_prix_epicerie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PrixEpicerie::Application.config.session_store :active_record_store
