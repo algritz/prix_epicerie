6af79024f66dc6d748dc4a0583aeee158ab1e49c
David Cloutier <algritz@videotron.ca>
2011-01-05 16:04:37 -0500

    fixed date display

--- app/helpers/produits_helper.rb
+++ app/helpers/produits_helper.rb
@@ -1,11 +1,15 @@
 module ProduitsHelper
+    def estMeilleurPrix(unProduit)
+        if unProduit == 1 then
+            couleurPolice = "#00FF00"
+        else
+            couleurPolice = "#a0a0a0"
+        end
+    end
 
-def estMeilleurPrix(unProduit)
-  if unProduit == 1 then
-    couleurPolice = "#00FF00"
-  else
-    couleurPolice = "#a0a0a0"
-  end
-end
+    def raccourcirDate(uneDate)
+        laDate = String(uneDate)
+        laDate = laDate[0..10]
+    end
 
 end
