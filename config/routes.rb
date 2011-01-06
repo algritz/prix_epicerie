4cd11c88d42d63ed0ab17dcd9e88fbc8d17853fe
David Cloutier <algritz@videotron.ca>
2011-01-06 09:06:33 -0500

    changed default routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 PrixEpicerie::Application.routes.draw do
   resources :produits
-
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +51,7 @@ PrixEpicerie::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "produits#index"
 
   # See how all your routes lay out with "rake routes"
 
